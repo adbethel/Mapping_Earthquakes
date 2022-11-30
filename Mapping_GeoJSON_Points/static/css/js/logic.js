781361a43defa1c91d662146f216437940abde4e
Amber Bethel <amberbethel@Ambers-MacBook-Air.local>
2022-11-29 20:34:43 -0500

    message

--- Mapping_GeoJSON_Points/static/css/js/logic.js
+++ Mapping_GeoJSON_Points/static/css/js/logic.js
@@ -1,30 +1,38 @@
-let map = L.map("mapid").setView([37.5, -122.5], 10);
+let map = L.map("mapid").setView([30, 30], 2);
 
 // Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"14",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
+//let sanFranAirport =
+//{"type":"FeatureCollection","features":[{
+    //"type":"Feature",
+    //"properties":{
+        //"id":"3469",
+        //"name":"San Francisco International Airport",
+        //"city":"San Francisco",
+        //"country":"United States",
+        //"faa":"SFO",
+        //"icao":"KSFO",
+        //"alt":"14",
+        //"tz-offset":"-8",
+        //"dst":"A",
+        //"tz":"America/Los_Angeles"},
+        //"geometry":{
+            //"type":"Point",
+            //"coordinates":[-122.375,37.61899948120117]}}
+//]};
+
+let streets = L.tileLayer(
+    attribution: 
+    maxZoon: 18,
+    accessToken: API_KEY
+);
+//we add out "streets" tile layer to the map
+streets.addTo(map)
+
+let airportData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/majorAirports.json";
 
 //Grabbing our GeoJSON data
-L.geoJSON(sanFranAirport{
-    //we turn each feature into a marker on the map
-    onEachFeature:function(feature, layer){
-        console.log(feature);
-        layer.bindPopup();
+L.geoJSON(airpotyData).then(function(data){
+        console.log(data);
+        L.geoJson(data).addTo(map);
     }
-}).addTo(map);
+});
